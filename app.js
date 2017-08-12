29ebb8705433c6e7604ba7da4187b65d1e1192bc
aikramer2 <aikramer2@gmail.com>
2017-08-12 10:28:36 -0700

    port

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express")
-
 const app = express()
+
+var port = process.env.PORT || 8080;
 
 app.get("/", (request, response) => {
   response.send("Hello")
 })
 
-app.listen(3000, () => {
-  console.log("Server listening on port 3000...")
+app.listen(port, () => {
+  console.log("Server listening on port: " + port)
 })
