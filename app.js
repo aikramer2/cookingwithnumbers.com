96793340221288a07f7c1fb6076356057a38ec92
aikramer2 <aikramer2@gmail.com>
2017-08-12 11:31:11 -0700

    update

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require("express")
+const path = require("path")
+
+// Init App
 const app = express()
 
+// Headers
+function TemplateData(){
+   this.title = "Cookingwithnumbers";
+ }
+
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+// Server Config
 var port = process.env.PORT || 8080;
 
+
+// Home Route
 app.get("/", (request, response) => {
-  response.send("Hello")
+  var header = new TemplateData()
+  response.render("index", header)
 })
 
+
+// Start Server
 app.listen(port, () => {
   console.log("Server listening on port: " + port)
 })
